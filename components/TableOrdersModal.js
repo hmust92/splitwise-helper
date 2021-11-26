070acfe140da6e6d257363e738c747c106b95437
Hasan <hasan.mustafa@taillight.com>
2021-11-26 07:33:46 -0600

    Adds ability to edit table orders

--- components/TableOrdersModal.js
+++ components/TableOrdersModal.js
@@ -1,13 +1,36 @@
+import { find, pipe, prop, propEq } from 'ramda';
+import { useRouter } from "next/router";
 import React from "react";
 
-import Input from './Input';
 import Modal from "./Modal";
 
-const TableOrderModals = ({ isOpen, onRequestClose, onSubmit }) => {
+const TableOrderModals = ({
+    isOpen,
+    onRequestClose,
+    onSubmit,
+    tableOrders,
+}) => {
+    const router = useRouter();
+
+    const id = router.query.params?.[1];
+
     const [tableOrderAmount, setTableOrderAmount] = React.useState("");
 
     return (
-        <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
+        <Modal
+            isOpen={isOpen}
+            onRequestClose={onRequestClose}
+            onAfterOpen={() => {
+                const initialAmount = pipe(
+                    find(propEq("id", id)),
+                    prop("amount")
+                )(tableOrders);
+
+                if (id && initialAmount) {
+                    setTableOrderAmount(initialAmount);
+                }
+            }}
+        >
             <div style={{ fontSize: 15 }}>Amount</div>
             <input
                 name="tableOrderAmount"
